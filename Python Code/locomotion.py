c5a6415ccc15be3ec08bca93fec4eea746048e9c
Don Lonsberry <donlonsberry@gmail.com>
2023-02-11 12:39:36 -0500

    Update locomotion.py

--- Python Code/locomotion.py
+++ Python Code/locomotion.py
@@ -1,74 +1,74 @@
 
 
 # import RPi.# GPIO as # GPIO 
+import gpiozero
 from time import sleep
 
-in1 = 24
-in2 = 23
+# in1 = 24
+# in2 = 23
 en = 25
 forgettabletempvarname=1
-
+# rightWheel = gpiozero.LED(24)
+# leftWheel = gpiozero.LED(23)
 # GPIO.setmode(# GPIO.BCM)
 # GPIO.setup(in1,# GPIO.OUT)
 # GPIO.setup(in2,# GPIO.OUT)
 # GPIO.setup(en,# GPIO.OUT)
-# GPIO.output(in1,# GPIO.LOW)
-# GPIO.output(in2,# GPIO.LOW)
+# rightWheel.OFF
+# leftWheel.OFF
 
+
+# TODO Must figure out this part instead of using LED.ON/OFF
 #p=# GPIO.PWM(en,1000)
+myMotor=gpiozero.Motor(23, 24)
+# if myMotor._check_open:
 #p.start(25)
 
 def movement(command):
     # moved this inside the method for obvi reasons
     # thankfully it wasn't still just named temp
     forgettabletempvarname=1
+    motorSpeed=1
 
-    if 'r' in command:
-        print("run")
-        if(forgettabletempvarname==1):
-            # GPIO.output(in1,# GPIO.HIGH)
-            # GPIO.output(in2,# GPIO.LOW)
-            print("forward")
-            
-        else:
-            # GPIO.output(in1,# GPIO.LOW)
-            # GPIO.output(in2,# GPIO.HIGH)
-            print("backward")
 
-    elif 's' in command:
+    if 's' in command:
         print("stop")
-        # GPIO.output(in1,# GPIO.LOW)
-        # GPIO.output(in2,# GPIO.LOW)
+        myMotor.stop
         
 
     elif 'f' in command:
         print("forward")
-        # GPIO.output(in1,# GPIO.HIGH)
-        # GPIO.output(in2,# GPIO.LOW)
+        myMotor.forward()
         forgettabletempvarname=1
         
 
     elif 'b' in command:
         print("back")
-        # GPIO.output(in1,# GPIO.LOW)
-        # GPIO.output(in2,# GPIO.HIGH)
+        myMotor.backward()
         forgettabletempvarname=0
         
 
     elif 'l' in command:
         print("low")
+        motorSpeed=0.25
         # p.ChangeDutyCycle(25)
        
 
     elif 'm' in command:
         print("medium")
+        motorSpeed=0.5
+
         # p.ChangeDutyCycle(50)
         
 
     elif 'h' in command:
         print("high")
+        motorSpeed=0.75
         # p.ChangeDutyCycle(75)
-        
+
+    #  TODO add the turning function
+    # I'm thinking just a 90 degree turn but not sure what that will look like until
+    # I interact with the motor    
     elif 'e' in command:
         # GPIO.cleanup()
 
@@ -82,6 +82,4 @@ def movement(command):
 # print("\n")
 # print("The default speed & direction of motor is LOW & Forward.....")
 # print("r-run s-stop f-forward b-backward l-low m-medium h-high e-ecommandit")
-# print("\n")  
-
-    
+# print("\n")
