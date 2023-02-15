90653662ebd138ed0405c832f9ffa0661ba98429
Don Lonsberry <donlonsberry@gmail.com>
2023-02-15 03:53:12 -0500

    Integrating tcp connectivity

--- Python Code/communications.py
+++ Python Code/communications.py
@@ -1,21 +1,74 @@
 # echo-server.py
 # testing
+import sys
 import socket
+import selectors
+import types
+import traceback
+from commslibs import Message
 
 HOST = ""  # Standard loopback interface address (localhost)
 PORT = 23232  # Port to listen on (non-privileged ports are > 1023)
-def listenforcommands():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ears:
-        ears.bind((HOST, PORT))
-        print('Listening...')
-        ears.listen()
-        conn, addr = ears.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = str(conn.recv(1024))
+
+chooser = selectors.DefaultSelector()
+
+# host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print(f"Listening on {(HOST, PORT)}")
+lsock.setblocking(False)
+
+chooser.register(lsock, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
+        message = Message(chooser, conn, addr)
+        chooser.register(conn, selectors.EVENT_READ, data=message)
+
+######
+
+try:
+    while True:
+
+        events = chooser.select(timeout=None)
+
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    Message.process_events(mask)
+                except Exception:
+                    print(
+                        f"Main: Error: Exception for {message.addr}:\n"
+                        f"{traceback.format_exc()}"
+                    )
+                    message.close()
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    chooser.close()
+
+
+
+
+
+#  def listenforcommands():
+#    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ears:
+#        ears.bind((HOST, PORT))
+#        print('Listening...')
+#        ears.listen()
+#        conn, addr = ears.accept()
+#        with conn:
+#            print(f"Connected by {addr}")
+#            while True:
+#                data = str(conn.recv(1024))
                 
-                if not data:
-                    break
-                return(data)
-                # conn.sendall(data)
+#                if not data:
+#                    break
+#                return(data)
+#                # conn.sendall(data)
