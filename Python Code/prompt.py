179138ac7d0157e749051d65a78cb9a4387b593c
Don Lonsberry <donlonsberry@gmail.com>
2023-02-16 12:40:23 -0500

    added sound to the prompt

--- Python Code/prompt.py
+++ Python Code/prompt.py
@@ -1,5 +1,8 @@
 #import communications
-from playsound import playsound
+#  from playsound import playsound
+#  import gps
+from sentience import getPositionData
+import mpyg321
 
 # from GUS import GUS
 def GUSPrompt(GUS):
@@ -10,9 +13,9 @@ def GUSPrompt(GUS):
     
     # print('GUSPrompt')
     
-    playsound('/Resources/sonar-a-dry-98689.mp3')
+    mpyg321('/Resources/sonar-a-dry-98689.mp3')
     stupidTempVar=str(GUS.locate(GUS))
-
+    #getPositionData(gps)
     query = input(stupidTempVar + "cm>'Position in Command Structure'>")
     #query = input()
     return (query)
--- Python Code/sentience.py
+++ Python Code/sentience.py
@@ -30,3 +30,10 @@ def Ping():
     return(round_distance)
 
 # TODO End of proximity sensor
+# ! GPS
+def getPositionData(gps):
+	nx = gpsd.next()
+	if nx['class'] == 'TPV':
+    	    latitude = getattr(nx, 'lat', "Unknown")
+                longitude = getattr(nx, 'lon', "Unknown")
+    	    print "Your position: lon = " + str(longitude) + ", lat = " + str(latitude)
