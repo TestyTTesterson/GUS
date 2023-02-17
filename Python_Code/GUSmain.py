0b7167ce605733368a4d601cbd312109a513cc50
Don Lonsberry <donlonsberry@gmail.com>
2023-02-17 02:09:30 -0500

    Update GUSmain.py

--- Python_Code/GUSmain.py
+++ Python_Code/GUSmain.py
@@ -4,9 +4,9 @@ from greeting import greeting
 import sentience as senses
 import locomotion as moveIt
 # import communications
-from gps3 import gps3
-the_connection = gps3.GPSDSocket() 
-the_fix = gps3.DataStream()
+from gps import gps
+the_connection = gps.connect()
+the_fix = gps.data()
 
 class GUSrobot:
     def __init__(self, name, location, avoision):
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         #brain(GUS)
         for new_data in the_connection:
             if new_data:
-                the_fix.refresh(new_data)
+                the_fix.(new_data)
             if not isinstance(the_fix.TPV['lat'], str): # lat as determinate of when data is 'valid'
                 speed = the_fix.TPV['speed']
                 latitude = the_fix.TPV['lat']
