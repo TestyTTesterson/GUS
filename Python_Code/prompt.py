cd6345f302162ed95a9309bab85df2e6f27151f1
Don Lonsberry <donlonsberry@gmail.com>
2023-02-17 07:49:57 -0500

    Update prompt.py

--- Python_Code/prompt.py
+++ Python_Code/prompt.py
@@ -6,12 +6,10 @@
 #tongue = mpyg321.MPyg321Player()
 import os
 #os.system("bluetoothctl connect FC:58:FA:9C:9C:08")
-
 #from playsound import playsound
-
-
 import serial
 serialPort = serial.Serial("/dev/serial0", 9600, timeout=0.5)
+import sounddevice as sd
 
 
 # from GUS import GUS
@@ -23,9 +21,11 @@ def GUSPrompt(GUS):
     if len(GPSlist) >= 5:
         print(GPSlist[3] + " " + GPSlist[4] + " " + GPSlist[5] + " " + GPSlist[6] + "\n")
 
-    print (os.getcwd())
+    print(sd.query_devices())
+    
+    print(sd.DeviceList)
     # this is where I run my bash script from because I remove the old repo first
-    os.system("mpg321 -B -q -g 12 GUS/Python_Code/Resources/sonar.mp3")
+    #os.system("mpg321 -B -q -g 12 GUS/Python_Code/Resources/sonar.mp3")
 
     stupidTempVar=str(GUS.locate(GUS))
 
