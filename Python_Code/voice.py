dc0f6e9424c1da3355641eb87fdaa3537611880c
Don Lonsberry <donlonsberry@gmail.com>
2023-02-23 06:43:13 -0500

    I been messing with everything

---
+++ Python_Code/voice.py
@@ -0,0 +1,5 @@
+from mpyg321 import MPyg321Player
+#from pygame import mixer # Load the popular external library
+from playsound import playsound
+voice = MPyg321Player.MPyg321Player()
+playsound('/Python_Code/Resources/voodoo-child.mp3')
