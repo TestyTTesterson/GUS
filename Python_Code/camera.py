6ea6018357815b1a72c7862962510826ea634429
Don Lonsberry <donlonsberry@gmail.com>
2023-05-23 12:11:28 -0400

    1st in a long while
    No idea what I last did

--- Python_Code/camera.py
+++ Python_Code/camera.py
@@ -1,12 +1,19 @@
-import pygame
-from pygame import camera
+import cv2
 
-from time import sleep
+cv2.namedWindow("GUS Vision")
+burst = cv2.VideoCapture(0)
 
-camera.init()
-camlist = camera.list_cameras()
+if burst.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
 
-cam = camera.Camera(camlist[0], (640, 480))
-if camlist:
-    image = cam.get_image()
-    image.save(image, "/homt/testyt/Pictures/testing123.jpg")
+while rval:
+    cv2.imshow("GUS Vision", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("GUS Vision")
